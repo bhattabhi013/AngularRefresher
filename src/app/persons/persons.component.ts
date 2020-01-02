5665c2f52d9ee5e42c7d891b73876a9506bfb9f8
bhattabhi013 <abhishek.bhatt@tatapower-ddl.com>
2020-01-02 18:07:49 +0530

    HTTP client

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -1,17 +1,29 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { PersonsService } from './persons.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-persons',
   templateUrl: './persons.component.html',
   styleUrls: ['./persons.component.css']
 })
-export class PersonsComponent {
+export class PersonsComponent implements OnDestroy, OnInit{
 
 @Input() personList: string [];
 @Input() playerList: any [];
-constructor( prsSerice: PersonsService) {
-  this.personList = prsSerice.persons;
+private personListSubs: Subscription;
+constructor( private prsService: PersonsService) {
+  this.personList = prsService.persons;
 }
-
+ngOnInit(){
+  this.personListSubs = this.prsService.personsChanged.subscribe(persons =>{
+    this.personList = persons;
+  });
+}
+removePerson(person : string){
+  this.prsService.removePerson(person);
+}
+ngOnDestroy(){
+ this.personListSubs.unsubscribe();
+}
 }
--- src/app/persons/persons.service.ts
+++ src/app/persons/persons.service.ts
@@ -1,13 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 
 export class PersonsService {
-  persons :string[] = ['MAX','MANUEL','ANNA'];
+  personsChanged = new Subject <string[]>();
+ public persons :string[] = ['MAX','MANUEL','ANNA'];
+ constructor( private http:HttpClient){}
+ fetchPersons(){
+   this.http.get<any>('https://swapi.co/api/people').subscribe();
+ }
 
   addPerson(name:string){
     this.persons.push(name);
+    this.personsChanged.next(this.persons);
     console.log("persons are "+this.persons);
+  }
+
+  removePerson(name: string){
+    this.persons = this.persons.filter(person=>{
+      return person !== name;
+    });
+    console.log("persons "+this.persons);
+    this.personsChanged.next(this.persons);
   }
 
 }
